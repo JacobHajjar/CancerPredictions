20958d291731ce061bf0bfd4ab07a1307ba59a9c
Jacob Hajjar <jacob.hajjar1@gmail.com>
2022-11-27 01:25:35 -0800

    finished plotting model

--- assignment1.py
+++ assignment1.py
@@ -16,8 +16,11 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
 from sklearn import metrics
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
 
-from numba import jit, cuda
 import matplotlib.pyplot as plt 
 import seaborn as sns
 
@@ -39,40 +42,65 @@ def main():
     "STDs:AIDS","STDs:HIV","STDs:Hepatitis B","Dx:HPV","STDs: Number of diagnosis"]]
 
     y_data = data_frame["Dx:Cancer"]
+
+
     #split trainingand testing
+    #x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size = 0.25, random_state = 0)
+    
+    #create models from data
+    log, svc_lin, knn, forest = model_data(x_data, y_data)
 
+    #create pipeline to scale data, then run model
+    sc = StandardScaler()
+    log_pipe = make_pipeline(sc, log)
+    svc_lin_pipe = make_pipeline(sc, svc_lin)
+    knn_pipe = make_pipeline(sc, knn)
+    forest_pipe = make_pipeline(sc, forest)
 
-    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size = 0.25, random_state = 0)
+    #create cross validation split
+    #cv = StratifiedShuffleSplit(n_splits=10, test_size=0.25, random_state=0)
 
-    #show data split
-    #print("% with cancer:")
-    #print(y_data[y_data == 1].shape)
+    #train and create cross validated prediction
+    #log_predictions = log.predict(x_test)
+    log_predictions = cross_val_predict(log_pipe, x_data, y_data, cv=2)
+    #svc_lin_predictions = svc_lin.predict(x_test)
+    svc_lin_predictions = cross_val_predict(svc_lin_pipe, x_data, y_data, cv=2)
+    #knn_predictions = knn.predict(x_test)
+    knn_predictions = cross_val_predict(knn_pipe, x_data, y_data, cv=2)
+    #forest_predictions = forest.predict(x_test)
+    forest_predictions = cross_val_predict(forest_pipe, x_data, y_data, cv=2)
+    
+    #print(cross_val_score(log_pipe, x_data, y_data, cv=cv))
+
+    #print_cm_classification_report("Logistic Regression", y_test, log_predictions)
+    print_cm_classification_report("Logistic Regression", y_data, log_predictions)
+    #print_cm_classification_report("Support Vector Machine Classifier", y_test, svc_lin_predictions)
+    print_cm_classification_report("Support Vector Machine Classifier", y_data, svc_lin_predictions)
+    #print_cm_classification_report("K-nearest neighbors", y_test, knn_predictions)
+    print_cm_classification_report("K-nearest neighbors", y_data, knn_predictions)
+    #print_cm_classification_report("Forest Classifier", y_test, forest_predictions)
+    print_cm_classification_report("Forest Classifier", y_data, forest_predictions)
+    plt.legend()
+
+    '''[["Age","Number of sexual partners","First sexual intercourse",
+    "Num of pregnancies","Smokes (years)","Smokes (packs/year)", "Hormonal Contraceptives (years)",
+    "IUD","IUD (years)","STDs","STDs (number)","STDs:condylomatosis","STDs:cervical condylomatosis","STDs:vaginal condylomatosis",
+    "STDs:vulvo-perineal condylomatosis","STDs:syphilis","STDs:pelvic inflammatory disease","STDs:genital herpes","STDs:molluscum contagiosum",
+    "STDs:AIDS","STDs:HIV","STDs:Hepatitis B","Dx:HPV","STDs: Number of diagnosis"]]
+    current_feature = data_frame[["Smokes (years)"]]
+    log.fit(current_feature / np.std(current_feature, 0), y_data)
+    print(log.coef_)'''
 
     #standardize data
     sc = StandardScaler()
-    x_train = sc.fit_transform(x_train)
-    x_test = sc.fit_transform(x_test)
-    
-    #create models from data
-    log, svc_lin, knn, forest = model_data(x_train, y_train)
-
-    log_predictions = log.predict(x_test)
-    svc_lin_predictions = svc_lin.predict(x_test)
-    knn_predictions = knn.predict(x_test)
-    forest_predictions = forest.predict(x_test)
-    print_cm_classification_report("Logistic Regression", y_test, log_predictions)
-    print_cm_classification_report("Support Vector Machine Classifier", y_test, svc_lin_predictions)
-    print_cm_classification_report("K-nearest neighbors", y_test, knn_predictions)
-    print_cm_classification_report("Forest Classifier", y_test, forest_predictions)
-    plt.legend()
+    x_data_scaled = sc.fit_transform(x_data)
+    log.fit(x_data_scaled, y_data)
+    coefs = pd.DataFrame(log.coef_.T, columns=['Coefficients'], index=x_data.columns)
+    coefs.plot(kind='barh', figsize=(9, 7))
+    plt.title('Ridge model')
+    plt.axvline(x=0, color='.5')
+    plt.subplots_adjust(left=.3)
     plt.show()
-
-    #print("prediction")
-    #predictions = chosen_model.predict(x_test)
-    #print(predictions)
-    #print("prediction probability")
-    #pred_prob = chosen_model.predict_proba(x_test)
-    #print(pred_prob)
     
     #print(y_test)
     #print(pred_prob)
@@ -104,31 +132,31 @@ def print_cm_classification_report(model_name, y_test, predictions):
     print()# Print a new line
     #Check precision, recall, f1-score
     print( classification_report(y_test, predictions, zero_division = 0))
-    plot_ROC_curve(model_name, y_test, predictions)
+    #plot_ROC_curve(model_name, y_test, predictions)
 
 def model_data(X_train, Y_train):
 
     #Using Logistic Regression 
     log = LogisticRegression(random_state = 0, n_jobs=8)
-    log.fit(X_train, Y_train)
+    #log.fit(X_train, Y_train)
 
     #Using SVC linear
     svc_lin = SVC(kernel = 'linear', random_state = 0, probability=True)
-    svc_lin.fit(X_train, Y_train)
+    #svc_lin.fit(X_train, Y_train)
 
     #Using KNeighborsClassifier 
     knn = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2, n_jobs=8)
-    knn.fit(X_train, Y_train)
+    #knn.fit(X_train, Y_train)
 
     #Using RandomForestClassifier method of ensemble class to use Random Forest Classification algorithm
     forest = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0, n_jobs=8)
-    forest.fit(X_train, Y_train)
+    #forest.fit(X_train, Y_train)
 
     #print model accuracy on the training data.
-    print('Logistic Regression Training Accuracy:', log.score(X_train, Y_train))
-    print('Support Vector Machine (Linear Classifier) Training Accuracy:', svc_lin.score(X_train, Y_train))
-    print('Random Forest Classifier Training Accuracy:', forest.score(X_train, Y_train))
-    print('K Nearest Neighbor Training Accuracy:', knn.score(X_train, Y_train))
+    #print('Logistic Regression Training Accuracy:', log.score(X_train, Y_train))
+    #print('Support Vector Machine (Linear Classifier) Training Accuracy:', svc_lin.score(X_train, Y_train))
+    #print('Random Forest Classifier Training Accuracy:', forest.score(X_train, Y_train))
+    #print('K Nearest Neighbor Training Accuracy:', knn.score(X_train, Y_train))
     return log, svc_lin, knn, forest
 
 if __name__ == '__main__':
